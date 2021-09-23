f79e7c37fb70e79abb515e2583c36a702e0ff215
Abrahan <ajorges8822@gmail.com>
2021-09-23 18:56:00 -0400

    hasta aca se tiene todo funcional los logins

--- app/src/main/java/com/example/proyect/fragments/HomeFragment.java
+++ app/src/main/java/com/example/proyect/fragments/HomeFragment.java
@@ -1,9 +1,11 @@
 package com.example.proyect.fragments;
 
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -11,6 +13,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -28,11 +32,24 @@ import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 
-public class HomeFragment extends Fragment {
+@RequiresApi(api = Build.VERSION_CODES.O)
+public class HomeFragment extends Fragment implements View.OnClickListener{
     //private FirebaseDatabase mdataBase;
     private DatabaseReference mdatabase,mdatabase1,mdataRef;
     private TextView mtvValue;
@@ -42,16 +59,15 @@ public class HomeFragment extends Fragment {
     private ValueEventListener eventListener;
     String serieSeleccionado="";
     String nombreSeleccionado;
-    int e=0;
+    int e=0;//contador para solucionar selector spinner
     //sector Graphi
     private TextView mprecio;
     LineGraphSeries<DataPoint> series1;
-    private double x,y,precio;
-
-    public HomeFragment() {
-        // Required empty public constructor
-    }
-
+    private double x,y,p,precio;
+    //fechas
+    Instant instant;
+    private RadioButton mchbD,mchbS,mchbM;
+    GraphView graph;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -61,47 +77,22 @@ public class HomeFragment extends Fragment {
         mtvValue=v.findViewById(R.id.tvfhValue);
         mprecio=v.findViewById(R.id.fptvPrecio);
         mdatabase=FirebaseDatabase.getInstance().getReference();
-        GraphView graph=v.findViewById(R.id.fpgraph);
+        graph=v.findViewById(R.id.fpgraph);
         mAuth=FirebaseAuth.getInstance();
         mdatabase1=FirebaseDatabase.getInstance().getReference("Consumos");
+        //sector radiobtn
+        mchbD=v.findViewById(R.id.fhrbtnD);
+        mchbS=v.findViewById(R.id.fhrbtnS);
+        mchbM=v.findViewById(R.id.fhrbtnM);
+        mchbD.setOnClickListener(this);
+        mchbS.setOnClickListener(this);
+        mchbM.setOnClickListener(this);
         //metodo para cargar datos
         loadDevices();
-        //sector graphi
 
-        x=0;
-        precio=0;
-        series1 =new LineGraphSeries<>();
-
-        Query query=mdatabase1.child("4F6SyZD9").orderByChild("fechaHora").startAt(1631160000).endAt(1631246399);
-        query.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-
-                for(DataSnapshot ds:snapshot.getChildren())
-                {
-                    double consumo=Double.parseDouble(ds.child("consumo").getValue().toString());
-                    y=consumo*1000;
-                    precio=precio+consumo;
-                    series1.appendData(new DataPoint(x,y),true,25);
-                    x=x+1;
-                }
-                precio=precio*0.82;//colocar el precio trayendo el dato de device precio
-                mprecio.setText("Bs"+String.format("%.2f",precio));
-                graph.getViewport().setXAxisBoundsManual(true);
-                graph.getViewport().setMinX(0);
-                graph.getViewport().setMaxX(23);
-                series1.setColor(Color.GREEN);
-                series1.setDrawDataPoints(true);
-                series1.setDataPointsRadius(11);
-                series1.setThickness(8);
-                graph.addSeries(series1);
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
+        cargarGraphic();
+        //sector graphi oncreate
+        cargarGraphic();
         graph.getGridLabelRenderer().setLabelFormatter(new DefaultLabelFormatter() {
             @Override
             public String formatLabel(double value, boolean isValueX) {
@@ -114,12 +105,15 @@ public class HomeFragment extends Fragment {
                 }
             }
         });
+        graph.getViewport().setXAxisBoundsManual(true);
+        graph.getViewport().setMinX(0);
+        graph.getViewport().setMaxX(23);
+        series1.setColor(Color.GREEN);
+        series1.setDrawDataPoints(true);
+        series1.setDataPointsRadius(11);
+        series1.setThickness(8);
 
-
-
-
-
-
+        //fin sector configuracion graph
         return v;
     }
 
@@ -136,6 +130,7 @@ public class HomeFragment extends Fragment {
                     {
                         String uidSerie=ds.child("nSerie").getValue().toString();
                         String nombre=ds.child("nombre").getValue().toString();
+                        precio=ds.child("preciokw").getValue(float.class);
                         devices.add(new Devices(uidSerie,nombre));
                     }
                     ArrayAdapter<Devices> arrayAdapter=new ArrayAdapter<>(getActivity(), R.layout.spinner_item,devices);
@@ -192,4 +187,55 @@ public class HomeFragment extends Fragment {
         });
         mdataRef.addValueEventListener(eventListener);
     }
+
+    public Date restarHoras(Date fecha, int horas){
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(fecha); // Configuramos la fecha que se recibe
+        calendar.add(Calendar.HOUR, horas);  // numero de horas a añadir, o restar en caso de horas<0
+        return calendar.getTime(); // Devuelve el objeto Date con las nuevas horas añadidas
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+
+            case R.id.fhrbtnD:
+
+                break;
+            case R.id.fhrbtnS:
+                break;
+            case R.id.fhrbtnM:
+                break;
+        }
+    }
+    public void cargarGraphic()
+    {
+        x=0;
+        p=0;
+        series1 =new LineGraphSeries<>();
+        String timeStampActual = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(Calendar.getInstance().getTime());
+        Query query=mdatabase1.child("4F6SyZD9").orderByChild("fechaHora").startAt(timeStampActual).endAt(1631246399);
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                for(DataSnapshot ds:snapshot.getChildren())
+                {
+                    double consumo=Double.parseDouble(ds.child("consumo").getValue().toString());
+                    y=consumo*1000;
+                    p=p+consumo;
+                    series1.appendData(new DataPoint(x,y),true,25);
+                    x=x+1;
+                }
+                p=p*precio;//colocar el precio trayendo el dato de device precio
+                mprecio.setText("Bs"+String.format("%.2f",p));
+                graph.addSeries(series1);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+    }
 }
+
